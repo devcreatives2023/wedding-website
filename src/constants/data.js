41e0e6bc91d67452f2ed2963ba55ac06b04a305d
abrar997 <abraralrawi997.com>
2022-09-27 16:56:29 +0300

    work

--- src/constants/data.js
+++ src/constants/data.js
@@ -11,7 +11,92 @@ const data = {
     { img: images.carousel4, name: "sassari", place: "Loream asl ksoinx " },
   ],
 
-  partners: [{ img: images.andre1, name: "", type: "", link: "" }],
+  partners: [
+    {
+      img: images.andre2,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.br1,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.andre1,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.checklist,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.r3,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.weddin1,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.ty2,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.ty5,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.rose2,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.rose5,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.rose6,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.ty1,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.andre6,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+    {
+      img: images.andre4,
+      name: "loream lalal",
+      type: ["john smith", "jody aboot", "sally micbraide ", "julia bendelton"],
+      link: "",
+    },
+  ],
 };
 
 export default data;
--- src/pages/Partners/Partners.js
+++ src/pages/Partners/Partners.js
@@ -1,18 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Navbar } from "../../components";
-
+import { data } from "../../constants";
+import { motion } from "framer-motion";
 const Partners = () => {
   return (
-    <div>
+    <motion.div className="m-4">
       <>
         <Navbar />
       </>
       <div className=" p-[5rem]">
-        <h1 className=" text-[4rem] px-[3rem] text-[#000000aa]">Professionisti </h1>
-        <h1 className=" top-[-100px] relative  text-center
-         text-[11rem] text-[#eee]">Professionisti </h1>
+        <h1 className=" text-[4rem] px-[3rem] text-[#000000aa]">
+          Professionisti
+        </h1>
+        <h1
+          className=" top-[-100px] relative  text-center
+         text-[11rem] text-[#eee] h-[100px]"
+        >
+          Professionisti
+        </h1>
       </div>
-    </div>
+      <div className=" px-[6rem] gap-[120px] grid grid-cols-1">
+        {data.partners.map((item, index) => {
+          return (
+            <div key={index} className=" grid grid-cols-4">
+              <img
+                src={item.img}
+                className="w-[280px] h-[280px] object-cover grid-cols-1"
+              />
+              <p className=" text-left font-sans mt-10 ml-5 
+              uppercase text-[1rem] font-bold text-[gray] grid-cols-1">{item.name} </p>
+
+              <ul className=" grid-cols-3 text-right flex flex-col text-[1rem] font-sans mt-[60px] gap-3 text-[grey] items-center">
+                {item.type.map((item, index) => {
+                  return (
+                    <li key={index} className=' capitalize font-semibold'>
+                      <Link to="">{item}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </motion.div>
   );
 };
 
